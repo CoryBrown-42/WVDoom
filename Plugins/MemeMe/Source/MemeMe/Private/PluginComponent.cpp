27fa1d8016eddf3e10245bf9706d6ab6325112dc
CoryBrown-42 <yroc.999@gmail.com>
2020-09-29 14:08:48 -0400

    Update PluginComponent.cpp

--- Plugins/MemeMe/Source/MemeMe/Private/PluginComponent.cpp
+++ Plugins/MemeMe/Source/MemeMe/Private/PluginComponent.cpp
@@ -1,7 +1,7 @@
 // Fill out your copyright notice in the Description page of Project Settings.
 
-#include "MemeMe.h"
 #include "PluginComponent.h"
+#include "MemeMe.h"
 
 // Sets default values
 APluginComponent::APluginComponent()
