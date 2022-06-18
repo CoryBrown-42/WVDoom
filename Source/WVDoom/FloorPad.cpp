fa45abf9113cee2d40ee8b0a05a4109d5aa85672
CoryB <cory@akuparagames.com>
2022-06-18 14:01:56 -0400

    Change from pixel style

--- Source/WVDoom/FloorPad.cpp
+++ Source/WVDoom/FloorPad.cpp
@@ -5,7 +5,8 @@
 
 #include "PlayerCharacter.h"
 
-// Sets default values
+// constructor
+// Sets default values for class
 AFloorPad::AFloorPad()
 {
  	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
